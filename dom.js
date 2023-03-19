56ad72c0278a6f756184334c821780357c6bc4d3
Saikumar-starboy <ksaikumar411@gmail.com>
2023-03-19 20:02:30 +0530

    commented out

--- dom.js
+++ dom.js
@@ -1,66 +1,66 @@
-//  console.log(document.domain);
-//  console.log(document.URL);
-//  console.log(document.title);
-//  console.log(document.doctype);
-//  console.log(document.head);
-//  console.log(document.body);
-//  console.log(document.all);
-//  console.log(document.forms);
+ console.log(document.domain);
+ console.log(document.URL);
+ console.log(document.title);
+ console.log(document.doctype);
+ console.log(document.head);
+ console.log(document.body);
+ console.log(document.all);
+ console.log(document.forms);
 
 
-// getelementbyid
-// console.log(document.getElementById('header-title'));
+getelementbyid
+console.log(document.getElementById('header-title'));
 
-// var headTitle = document.getElementById('header-title');
+var headTitle = document.getElementById('header-title');
 
-// console.log(headTitle);
-// headTitle.textContent = 'Hello';
-// headTitle.innerText = 'goodbye';
-// headTitle.innerHTML = '<h3>Hell0</h3>';;
-// header.style.borderBottom = 'solid 3px #000';
-// console.log(headTitle.innerText);
+console.log(headTitle);
+headTitle.textContent = 'Hello';
+headTitle.innerText = 'goodbye';
+headTitle.innerHTML = '<h3>Hell0</h3>';;
+header.style.borderBottom = 'solid 3px #000';
+console.log(headTitle.innerText);
 
-// getelementbyclassname
+getelementbyclassname
 
-// var items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent = 'hellp';
-// items[1].style.fontweight = 'bold';
-// items[1].style.background = 'yellow';
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'hellp';
+items[1].style.fontweight = 'bold';
+items[1].style.background = 'yellow';
 
-// for(var i=0;i<items.length;i++){
-//    items[i].style.backgroundColor = '#f4f4f4';
-// }
+for(var i=0;i<items.length;i++){
+   items[i].style.backgroundColor = '#f4f4f4';
+}
 
-// queryselector
+queryselector
 
-//  var header = document.querySelector('#main-header');
-//  header.style.borderBottom = 'solid 4px #ccc';
+ var header = document.querySelector('#main-header');
+ header.style.borderBottom = 'solid 4px #ccc';
 
-//  var input = document.querySelector('input');
-//  input.value = 'HEllo world';
+ var input = document.querySelector('input');
+ input.value = 'HEllo world';
 
-//  var submit = document.querySelector('input[type="submit"]');
-//  submit.value="SEND";
+ var submit = document.querySelector('input[type="submit"]');
+ submit.value="SEND";
 
-//  var item = document.querySelector('.list-group-item');
-//  item.style.color = 'red';
+ var item = document.querySelector('.list-group-item');
+ item.style.color = 'red';
 
-//  var lastItem = document.querySelector('.list-group-item:last-child');
-//  lastItem.style.color = 'blue';
+ var lastItem = document.querySelector('.list-group-item:last-child');
+ lastItem.style.color = 'blue';
 
-// queryselector all //
+queryselector all //
 
-// var titles = document.querySelectorAll('.title');
+var titles = document.querySelectorAll('.title');
 
-// console.log(titles);
-// titles[0].textContent = 'Hello';
+console.log(titles);
+titles[0].textContent = 'Hello';
 
-// var odd = document.querySelectorAll('li:nth-child(odd)');
-// var even = document.querySelectorAll('li:nth-child(even)');
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
 
-// for(var i=0;i<odd.length;i++){
-//     odd[i].style.backgroundColor = '#f4f4f4';
-//     even[i].style.backgroundColor = '#ccc';
-//  }
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
+ }
