3e80d07e8b72270ae0352ec351c76db4ba0ec48a
Saikumar-starboy <ksaikumar411@gmail.com>
2023-03-25 22:12:44 +0530

    Delete and Edit functionality

--- demo.js
+++ demo.js
@@ -1,26 +1,58 @@
-var newDiv = document.createElement('div');
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
-newDiv.className='Hello';
-newDiv.id = 'hello1';
+//form submit event 
+form.addEventListener('submit' , addItem);
 
-newDiv.setAttribute('title','Hello div');
+itemList.addEventListener('click', removeItem);
 
-var newDivText = document.createTextNode('HEllo ');
+filter.addEventListener('keyup',filterItems);
 
-newDiv.appendChild(newDivText);
+// add item 
+function addItem(e){
+    e.preventDefault();
+     
+    var newItem = document.getElementById('item').value;
 
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
+    var li = document.createElement('li');
 
-container.insertBefore(newDiv,h1);
+    li.className = 'list-group-item';
 
-console.log(newDiv);
+    li.appendChild(document.createTextNode(newItem));
 
-// adding hello before item1
-var parentNode = document.getElementById('items');
-var element = parentNode.innerHTML;
-var text = '<li>HELLo</li>';
+    var deleteBtn = document.createElement('button');
 
-parentNode.innerHTML = text + parentNode.innerHTML;
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
+    deleteBtn.appendChild(document.createTextNode('X'));
 
+    li.appendChild(deleteBtn);
+
+    itemList.appendChild(li);
+}
+
+function removeItem(e){
+     if(e.target.classList.contains('delete')){
+        if(confirm('Are you Sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+     }
+}
+
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+             item.style.display = 'block';
+            }else {
+                item.style.display = 'none';
+            }
+    });
+    
+}
